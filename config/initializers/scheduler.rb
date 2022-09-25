5f260d7f56a1b48733c457664b36d2ef53357515
usernaimandrey <anshlyapnikov@yandex.ru>
2022-09-25 05:12:16 +0300

    add_db_pg

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,12 +2,12 @@
 
 # require 'rufus-scheduler'
 
-# scheduler = Rufus::Scheduler.new
+scheduler = Rufus::Scheduler.new
 
-# scheduler.every '1h' do
-#   FetchWeatherDataJob.perform_later("#{URL_API}/#{CITY_ID}/#{API_REFERENCE}?apikey=#{API_KEY}&language=#{I18n.default_locale}")
-# end
+scheduler.every '1h' do
+  FetchWeatherDataJob.perform_later("#{URL_API}/#{CITY_ID}/#{API_REFERENCE}?apikey=#{API_KEY}&language=#{I18n.default_locale}")
+end
 
-# scheduler.every '1h10m' do
-#   CleanUpOldRecordJob.perform_later
-# end
+scheduler.every '1h10m' do
+  CleanUpOldRecordJob.perform_later
+end
